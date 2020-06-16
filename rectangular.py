5d659b60319527729ce018695cdf1632a6c12cba
Mobin Nik_khesal <mobinnikkhesal102@gmail.com>
2020-06-16 18:42:11 +0430

    final revision

--- rectangular.py
+++ rectangular.py
@@ -82,11 +82,7 @@ class Rectangular():
         else:
             self.overlap_height = (self.bottom_rect.y + self.bottom_rect.height) - self.top_rect.y
         
-        # print(self.area)
-        # print(other.area)
-        # print(30*"#")
-        # print(self.overlap_width)
-        # print(self.overlap_height)
+
         if self.overlap_width > 0 and self.overlap_height > 0:
             self.intersection_area = self.overlap_width * self.overlap_height / self.area
             other.intersection_area = self.overlap_width * self.overlap_height / self.area 
@@ -128,73 +124,29 @@ if __name__ == "__main__":
 
 
 
-# class Rectangular():
-#     def __init__(self, x, y, width, height,time=None):
-#         """
-#         description: 
-#             difine a rectangular in X-Y coordinate
-#         args:
-#             x: horizontal coordinate
-#             y: vertical coordinate 
-#             width: the segment between x, x+ width 
-#             height: the segment between y, y+height
-#             time: timeStamp of the creation of this object
-#         """
-#         self.x = x
-#         self.y = y
-#         self.width = width
-#         self.height = height
 
-#         now = datetime.now()
-#         dt_string = now.strftime("%Y-%m-%d %H:%M:%S")
-#         self.time = dt_string
-    
-#     def _has_intersection(self,other):
-#         """
-#         description: 
-#             Check that the intersection of two objects(Intersection is symmetric).
-#             if just of vertex in second rectangular in main rectangular 
-#             then they have an intersection.
-#             WARNING: this definition excludes the boundary.
-#         args:
-#             Take a instace of Rectangular Class
-#         Return(boolen): 
-#             True if there exist a intersection Area
-#         """
+# def _has_intersection(self,other):
+#     """
+#     description: 
+#         Check that the intersection of two objects(Intersection is symmetric).
+#         if just of vertex in second rectangular in main rectangular 
+#         then they have an intersection.
+#         WARNING: this definition excludes the boundary.
+#     args:
+#         Take a instace of Rectangular Class
+#     Return(boolen): 
+#         True if there exist a intersection Area
+#     """
 
-#         x1 = (self.x < other.x < (self.x+ self.width)) 
-#         x2 = (self.x < (other.x + other.width) < (self.x+ self.width))
-#         y1 = (self.y < other.y < (self.y + self.height))
-#         y2 = (self.y < (other.y + other.height) < (self.y + self.height))
-#         #these are four boolean that indcate the existence of vertices in main rectangular 
-#         x1y1 = (x1 and y1)
-#         x2y1 = (x2 and y1)
-#         x1y2 = (x1 and y2)
-#         x2y2 = (x2 and y2)
-#         if any([x1y1, x2y1, x1y2, x2y2]):
-#             return True
-#         return False
-
-#     def filter_accepted_rectangular(self, others):
-#         """
-#         description: 
-#             Select thoses rectangular that has intersection with main rectangular
-#         args:
-#             Take a instace of Rectangular Class
-#         Return: 
-#             List of dictionay of accepted Rectangular
-#         """
-#         accepted_list = []
-#         for item in others:
-#             if self._has_intersection(item):
-#                 accepted_list.append(item.json_able())
-#         return accepted_list
-    
-#     def json_able(self):
-#         """
-#         description: 
-#             it return just a dictionary of class attributes
-#         """
-        
-#         ret = {"x":self.x, "y":self.y, "width":self.width, "height":self.height,"time":self.time}
-#         return ret
+#     x1 = (self.x < other.x < (self.x+ self.width)) 
+#     x2 = (self.x < (other.x + other.width) < (self.x+ self.width))
+#     y1 = (self.y < other.y < (self.y + self.height))
+#     y2 = (self.y < (other.y + other.height) < (self.y + self.height))
+#     #these are four boolean that indcate the existence of vertices in main rectangular 
+#     x1y1 = (x1 and y1)
+#     x2y1 = (x2 and y1)
+#     x1y2 = (x1 and y2)
+#     x2y2 = (x2 and y2)
+#     if any([x1y1, x2y1, x1y2, x2y2]):
+#         return True
+#     return False
